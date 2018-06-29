fc1242f9cc261af79f92b54a78e83212f6d80111
Handoyo <dyo.9913@gmail.com>
2018-06-29 17:37:16 +0700

    test lagi

--- main.go
+++ main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-	// "os"
+	"os"
 	"github.com/hxline/pgmuxsem-master/service"
 )
 
-const (
-	TEST_DB_HOST     = "10.17.50.48"
-	TEST_DB_PORT     = "5432"
-	TEST_DB_USERNAME = "postgres"
-	TEST_DB_PASSWORD = "postgres"
-	TEST_DB_NAME     = "hxline"
-)
+// const (
+// 	TEST_DB_HOST     = "10.17.50.48"
+// 	TEST_DB_PORT     = "5432"
+// 	TEST_DB_USERNAME = "postgres"
+// 	TEST_DB_PASSWORD = "postgres"
+// 	TEST_DB_NAME     = "hxline"
+// )
 func main() {
 	a := service.App{}
-	a.Initialize(TEST_DB_USERNAME, TEST_DB_PASSWORD, TEST_DB_NAME)
+	// a.Initialize(TEST_DB_USERNAME, TEST_DB_PASSWORD, TEST_DB_NAME)
 
-	// a.Initialize(
-	// 	os.Getenv("APP_DB_USERNAME"),
-	// 	os.Getenv("APP_DB_PASSWORD"),
-	// 	os.Getenv("APP_DB_NAME"))
+	a.Initialize(
+		os.Getenv("APP_DB_USERNAME"),
+		os.Getenv("APP_DB_PASSWORD"),
+		os.Getenv("APP_DB_NAME"))
 
 	a.Run(":8080")
 }
